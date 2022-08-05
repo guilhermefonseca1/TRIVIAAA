cf99db50bc1d78f5d8175aeef5bbec54e35d4361
Lucas Carvalho <drigerm@hotmail.com>
2022-08-05 17:45:08 -0300

    pra mergear

--- src/pages/Config.js
+++ src/pages/Config.js
@@ -5,6 +5,25 @@ class Config extends React.Component {
     return (
       <div>
         <h1 data-testid="settings-title">Configurações</h1>
+        <form>
+          <label htmlFor="dificuldade">
+            Dificuldade:
+            <select
+              name="dificuldade"
+              id="dificuldade"
+            >
+              <option>Easy</option>
+              <option>Medium</option>
+              <option>Hard</option>
+            </select>
+          </label>
+          <label htmlFor="sound">
+            <select name="sound" id="sound">
+              <option>On</option>
+              <option>Off</option>
+            </select>
+          </label>
+        </form>
       </div>
     );
   }
