acfc594ec1f1eb00dd75d2631258baa5e32abb3a
Guilherme Soares <guilherme.limasoares@hotmail.com>
2022-08-05 17:05:47 -0300

    1. Crie a tela de login, onde a pessoa que joga deve preencher as informações para iniciar um jogo

---
+++ src/Pages/Login.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import logo from '../trivia.png';
+
+export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      inputName: '',
+      inputEmail: '',
+
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+
+    });
+  }
+
+  render() {
+    const { inputEmail, inputName } = this.state;
+    return (
+      <div>
+        <div className="App">
+          <header className="App-header">
+            <img src={ logo } className="App-logo" alt="logo" />
+            <p>SUA VEZ</p>
+          </header>
+
+        </div>
+        <form>
+          <label htmlFor="playerName">
+            <input
+              id="playerName"
+              data-testid="input-player-name"
+              value={ inputName }
+              onChange={ this.handleChange }
+              name="inputName"
+              type="text"
+            />
+          </label>
+          <label htmlFor="email">
+            <input
+              id="email"
+              data-testid="input-gravatar-email"
+              value={ inputEmail }
+              onChange={ this.handleChange }
+              name="inputEmail"
+              type="email"
+            />
+          </label>
+          <button
+            data-testid="btn-play"
+            type="button"
+            disabled={ !(inputName && inputEmail) }
+            onClick={ this.handleClick }
+          >
+            Play
+          </button>
+
+        </form>
+      </div>
+    );
+  }
+}
