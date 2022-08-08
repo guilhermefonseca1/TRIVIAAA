de9be7e8c98c3c10008305d13f0db533aa5fd7ac
Artur Vidor <vidorartur@gmail.com>
2022-08-08 14:09:41 -0300

    req 6 concluido - arrumando o merge

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -3,43 +3,43 @@ import { connect } from 'react-redux';
 import md5 from 'crypto-js/md5';
 import PropTypes from 'prop-types';
 
-// class Feedback extends React.Component {
-//   constructor() {
-//     super();
+class Feedback extends React.Component {
+  constructor() {
+    super();
 
-//     this.state = {
-//       gravatarImageUrl: '',
-//     };
-//   }
+    this.state = {
+      gravatarImageUrl: '',
+    };
+  }
 
-//   componentDidMount() {
-//     const { email } = this.props;
-//     const UrlEnd = md5(email).toString();
-//     this.setState({
-//       gravatarImageUrl: `https://www.gravatar.com/avatar/${UrlEnd}`,
-//     });
-//   }
+  componentDidMount() {
+    const { email } = this.props;
+    const UrlEnd = md5(email).toString();
+    this.setState({
+      gravatarImageUrl: `https://www.gravatar.com/avatar/${UrlEnd}`,
+    });
+  }
 
-//   render() {
-//     const { gravatarImageUrl } = this.state;
-//     const { username, scores } = this.props;
-//     const altText = `Imagem de ${username}`;
-//     return (
-//       <div>
-//         <header>
-//           <h1>header</h1>
-//           <img
-//             src={ gravatarImageUrl }
-//             data-testid="header-profile-picture"
-//             alt={ altText }
-//           />
-//           <p data-testid="header-player-name">{ username }</p>
-//           <p data-testid="header-score">{ scores }</p>
-//         </header>
-//       </div>
-//     );
-//   }
-// }
+  render() {
+    const { gravatarImageUrl } = this.state;
+    const { username, scores } = this.props;
+    const altText = `Imagem de ${username}`;
+    return (
+      <div>
+        <header>
+          <h1>header</h1>
+          <img
+            src={ gravatarImageUrl }
+            data-testid="header-profile-picture"
+            alt={ altText }
+          />
+          <p data-testid="header-player-name">{ username }</p>
+          <p data-testid="header-score">{ scores }</p>
+        </header>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = (store) => ({
   username: store.player.name,
@@ -48,10 +48,10 @@ const mapStateToProps = (store) => ({
 
 });
 
-// Feedback.propTypes = {
-//   username: PropTypes.string.isRequired,
-//   scores: PropTypes.number.isRequired,
-//   email: PropTypes.string.isRequired,
-// };
+Feedback.propTypes = {
+  username: PropTypes.string.isRequired,
+  scores: PropTypes.number.isRequired,
+  email: PropTypes.string.isRequired,
+};
 
-// export default connect(mapStateToProps)(Feedback);
+export default connect(mapStateToProps)(Feedback);
