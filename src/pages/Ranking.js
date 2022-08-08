6e5e5bd3582884bc2e71e73ea2ef24da885f983e
Breno Prado <breno.o.prado@hotmail.com>
2022-08-08 15:43:05 -0300

    Inicio do requisito 19 :turtle:

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -4,20 +4,38 @@ import propTypes from 'prop-types';
 class Ranking extends React.Component {
   render() {
     const { history } = this.props;
+    const ranking = localStorage.getItem('ranking');
     return (
       <div>
         <header>
           <h2 data-testid="ranking-title">Ranking</h2>
         </header>
-        <form>
-          <button
-            type="button"
-            data-testid="btn-go-home"
-            onClick={ history.push('/') }
-          >
-            Voltar para tela de Login
-          </button>
-        </form>
+        <main>
+          {
+            (ranking.length > 0)
+              ? (
+                <ol>
+                  {
+                    ranking.map((person, index) => (
+                      <li key={ index }>
+                        <img src={ person.picture } alt="imagem da pessoa jogadora" />
+                        <h4>{person.name}</h4>
+                        <p>{person.score}</p>
+                      </li>
+                    ))
+                  }
+                </ol>
+              )
+              : <p>Não há jogadores no ranking ainda</p>
+          }
+        </main>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ history.push('/') }
+        >
+          Voltar para tela de Login
+        </button>
       </div>
     );
   }
